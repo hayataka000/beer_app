4bc526c6bb90e5eecd776fff500ffd18a77130fe
hayataka000 <hysdtksg@gmail.com>
2019-08-10 14:32:45 +0900

    reviews　新規投稿機能実装

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ gem 'erb2haml'
 gem 'better_errors'
 gem 'binding_of_caller'
 gem "font-awesome-rails"
+gem 'pry-rails'
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :maker, :text, :sharpness, :richness, :sourness, :bitterness, :sweetness)
+  end
 end

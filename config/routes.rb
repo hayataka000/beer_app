ff11abbee88d5263631b42f994967d743307e582
hayataka000 <hysdtksg@gmail.com>
2019-08-16 14:20:08 +0900

    header ドロップダウン

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top_page#index'
-
   resources :reviews, only: [:index,:new,:create,:show, :edit, :update, :destroy]
   resources :mypages, only: [:index]
   resources :search, only: [:index] do
